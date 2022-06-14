bff4c1ade79ff66c9e5ec13539c94f39b0548ffd
EdmerFco <83522812+EdmerFco@users.noreply.github.com>
2022-06-14 15:41:22 -0500

    IMSS
    Se desarrollo el calculo del IMSS con el UMA de 96.22

---
+++ IMSS/IMSS/Aportaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSS
+{
+    public struct Aportaciones
+    {
+        public Aportaciones( decimal EnfermedadMaternidad, decimal InvalidezVida, decimal Retiro, decimal Cesantia, decimal Infonavit)
+        {
+            EnFer = EnfermedadMaternidad;
+            Inc = InvalidezVida;
+            Ret = Retiro;
+            Ces = Cesantia;
+            InF = Infonavit;
+
+        }
+        public decimal EnFer{ get; set; }
+        public decimal Inc{ get; set; }
+        public decimal Ret{ get; set; }
+        public decimal Ces{ get; set; }
+        public decimal InF{ get; set; }
+
+    }
+}
---
+++ IMSS/IMSS/CalculadoraIMSS.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSS
+{
+    public class CalculadoraIMSS
+    {
+         public static void  Presentacion()
+        {
+            decimal SBC;
+            decimal UMAS;
+            int Rol; 
+
+            Console.WriteLine("Calculadora IMSS");
+            Console.WriteLine("Para calcular los Aportaciones de Patrones coloca le numero 1");
+            Console.WriteLine("Para calcular los Aportaciones de Trabajador coloca le numero 2");
+            Rol = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa la SBC");
+            SBC = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("ingresa la UMAS");
+            UMAS = decimal.Parse(Console.ReadLine());
+
+            Aportaciones aport = new Aportaciones(); 
+            aport = calcular(SBC, UMAS, Rol);
+
+            Console.WriteLine($"Enfermedades y Maternidad: \n{aport.EnFer}");
+            Console.WriteLine($"Invalidez y Vida: \n{aport.Inc}");
+            Console.WriteLine($"Retiro:  \n{aport.Ret}");
+            Console.WriteLine($"Cesantia: \n{aport.Ces}");
+            Console.WriteLine($"Credito Infonavit: \n{aport.InF}"); 
+
+
+        }
+        public static Aportaciones calcular(decimal SBC, decimal UMAS, int Rol)
+        {
+            Aportaciones Apor = new Aportaciones();
+
+            if (Rol == 1)
+            {
+                Apor.EnFer = (SBC - (3m * UMAS)* 0.011m);
+                Apor.Inc =   (SBC * 0.0175m);
+                Apor.Ret =   (SBC * 0.02m);
+                Apor.Ces =   (SBC * 0.0315m);
+                Apor.InF =   (SBC * 0.5m);
+            }
+            else if (Rol == 2)
+            {
+                Apor.EnFer = (SBC - (3m * UMAS)* 0.004m);
+                Apor.Inc =   (SBC * 0.00625m);
+                Apor.Ret =   (SBC * 0m);
+                Apor.Ces =   (SBC * 0.0125m);
+                Apor.InF =   (SBC * 0m);
+            }
+            else
+                Console.WriteLine("no se puede realisar El Calcular intente de nuevo con una numeracion correcta");
+            
+            return Apor;
+        }
+    }
+}
